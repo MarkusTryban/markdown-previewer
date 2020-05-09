bd6fd9386154aed8c8c33925eee6e10389f0ce97
MarkusTryban <markustryban@gmail.com>
2020-05-09 17:05:11 -0400

    Update MarkdownPreview.js

--- src/MarkdownPreview.js
+++ src/MarkdownPreview.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 
+const markdownTest = ' ### Headers # Header 1';
+
 class MarkdownPreview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: markdownTest,
+    };
+  }
+
   render() {
+    const { markdown } = this.state;
+
     return (
       <div id='main'>
+        <h1>Markdown Previewer</h1>
         <div className='containers'>
           <div className='container'>
-            <textarea id='editor' className='container'></textarea>
+            <textarea id='editor' className='container' />
           </div>
-          <div id='preview' className='container'></div>
+          <div id='preview' className='container'>
+            {MarkdownPreview(markdown)}
+          </div>
         </div>
       </div>
     );
   }
 }
+
+export default MarkdownPreview;
